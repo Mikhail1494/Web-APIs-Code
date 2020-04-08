f7c663e00dee1fd7f489c20d1ffc1e41339389d4
Mikhail Mednikh <mmednikh@gmail.com>
2020-04-07 20:51:16 -0400

    chenged JS code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //Dom element
 var currentQuestion = 0;
 var score = 0;
-var ticeker = 50; 
+var ticeker = 50;
 var totquestions = questions.length;
-var quizContainer = document.getElementById("question-box");
+var quizContainer = document.querySelector(".question-box");
+console.log("quizContainerStart: ", quizContainer);
 var questionEl = document.getElementById("questions");
 var selection1 = document.getElementById("selection1");
 var selection2 = document.getElementById("selection2");
@@ -17,45 +18,64 @@ const CORRECT_COUNT = 1;
 const MAX_QUESTIONS = 3;
 const scoreList = 0;
 //Choice a question
-function takeQuestion (questionIndex){
-    document.getElementById("go").disabled = true;
-    var q = questions[questionIndex]
-    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
-    selection1.textContent = q.selection1;
-    selection2.textContent = q.selection2;
-    selection3.textContent = q.selection3;
-};
+function takeQuestion(questionIndex) {
+  document.getElementById("go").disabled = true;
+  var q = questions[questionIndex];
+  if (q) {
+  }
+  questionEl.textContent = questionIndex + 1 + ". " + q.question;
+  selection1.textContent = q.selection1;
+  selection2.textContent = q.selection2;
+  selection3.textContent = q.selection3;
+}
 //next step to question
-function takeNextQuestion(){
-    var userChoice = document.querySelector('input[type=radio]:checked');
-    if(!userChoice){
-        alert("Mark your answer");
-        return;
-    }
-    var answer = userChoice.value;
-    if(questions[currentQuestion].answer === answer){
-        score += 1;
-    }
+function takeNextQuestion() {
+  console.log("total_questions: ", totquestions);
+  var userChoice = document.querySelector("input[type=radio]:checked");
+  var answer = userChoice.value;
+
+  if (questions[currentQuestion].answer === answer) {
+    score++;
     userChoice.checked = false;
-    currentQuestion++;
-    if (currentQuestion == totquestions){
-        startBtn.style.visibility='hidden';
-        nextBtn.style.visibility='hidden';
-        quizContainer.style.display="none";
-        result.style.display="";
-        result.textContent = score;
-        return;
+    console.log("right answer");
+    console.log("currentQuestionNumber: ", currentQuestion);
+    if (currentQuestion == totquestions - 1) {
+      console.log("no more questions");
+      startBtn.style.visibility = "hidden";
+      nextBtn.style.visibility = "hidden";
+      console.log(quizContainer);
+      quizContainer.style.display = "none";
+    //   result.style.display = "";
+    //   result.textContent = score;
+      return;
     }
-    takeQuestion(currentQuestion);
+  } else if (!userChoice) {
+    alert("Mark your answer");
+    console.log("no answer");
+    //return;
+  } else {
+    alert("Wrong answer!");
+  }
+  // if(questions[currentQuestion].answer === answer){
+  //     score++;
+  //     userChoice.checked = false;
+  // }
+  //userChoice.checked = false;
+  //currentQuestion++;
+  //console.log(currentQuestion)
+
+  console.log("currentQuestionNumberAfterAnswer", currentQuestion);
+  currentQuestion++;
+  takeQuestion(currentQuestion);
 }
 //ticker
-var myTimer
-function clock(){
-    myTimer = setInterval(myCloack, 1000);
-    function myCloack(){
-        document.getElementById("ticker").innerHTML = ticeker --;
-        if (ticeker == 0) {
-            clearInterval(myTimer);
-        }
+var myTimer;
+function clock() {
+  myTimer = setInterval(myCloack, 1000);
+  function myCloack() {
+    document.getElementById("ticker").innerHTML = ticeker--;
+    if (ticeker == 0) {
+      clearInterval(myTimer);
     }
+  }
 }
